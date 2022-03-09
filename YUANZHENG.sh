dfa47dfb2be7ca7fcdb0d8b93e4ec405086769c3
楊遠徵 <codes@189.cn>
2022-03-10 00:18:07 +0800

    YUANZHENG

---
+++ YUANZHENG.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 克隆源码
+git clone https://github.com/unifreq/openwrt_packit packit
+mv -n packit/* ./ ; rm -Rf packit
+
+# 修改作者
+sed -i 's/ophub/womade/g' action.yml
+
+# 自定义 banner
+wget https://github.com/womade/LEDE_actions/raw/main/modify/etc/banner -O files/banner
+sed -i '/Base on OpenWrt/,+5d' public_funcs
+
+# 修改仓库
+sed -i 's/unifreq/womade/g' openwrt_flippy.sh
+sed -i 's/master/main/g' openwrt_flippy.sh
+
+# WHOAMI
+sed -i 's/flippy/YY-ZHENG/g' openwrt_flippy.sh
+
+# KERNEL VERSION
+sed -i 's/-flippy-/-yuanzheng-/g' make.env
+
+exit 0
